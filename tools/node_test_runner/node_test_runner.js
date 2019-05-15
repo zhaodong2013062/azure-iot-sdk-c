2e01986465f4067d733b3bffca90a14e63895823
jebrando <jelani.brandon@microsoft.com>
2019-05-15 16:58:04 -0700

    Update node test runner to launch an application

--- tools/node_test_runner/node_test_runner.js
+++ tools/node_test_runner/node_test_runner.js
@@ -5,23 +5,43 @@
 var prov_svc_client = require("./node_prov_service.js")
 var uuid = require('uuid');
 
-const { spawn } = require('child_process');
+const { exec } = require('child_process');
 
-function execute_test_process(test_process, cmd_args)
-{
-    const test_ps = spawn(test_process, cmd_args);
-
-    //ls.stdout.on('data', (data) => {
-     // console.log(`stdout: ${data}`);
-    //});
-
-    //ls.stderr.on('data', (data) => {
-    //  console.log(`stderr: ${data}`);
-    //});
-
-    test_ps.on('close', (code) => {
-      console.log(`child process exited with code ${code}`);
+function execute_test_process(test_process, cmd_args, result_cb) {
+    // Start the process
+    exec(test_process, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`process return non zero exit code error: ${error}`);
+            result = 1;
+        }
+        else {
+            result = 0;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+        return result_cb(result);
     });
+}
+
+function register_dps_enrollment(test_type, conn_string, device_id, webhookuri, argv) {
+    if (test_type == "x509") {
+        var x509_cert = argv.x509Cert;
+        if (typeof x509_cert == 'undefined' || x509_cert.length == 0) {
+            console.log("Invalid x509 certificate specified");
+        }
+        else {
+            prov_svc_client.create_x509_registration(conn_string, device_id, x509_cert, webhookuri);
+        }
+    }
+    else if (test_type == "symm_key") {
+        // Don't check the symmetric key if it's not there
+        // then the service will create one
+        var symm_key = argv.symmKey;
+        prov_svc_client.create_symm_key_registration(conn_string, device_id, symm_key, webhookuri);
+    }
+    else if (test_type == "tpm") {
+        prov_svc_client.create_tpm_registration(conn_string, device_id, webhookuri);
+    }
 }
 
 var argv = require('yargs')
@@ -62,13 +82,20 @@ var argv = require('yargs')
         type: 'string',
         demandOption: false
     })
+    .option('testexe', {
+        alias: 'e',
+        describe: 'The full path of the application to execute',
+        type: 'string',
+        demandOption: false
+    })
     .argv;
 
 var device_id = argv.deviceid;
 var conn_string = argv.connectionstring;
 var webhookuri = argv.webhookurl;
 var test_type = argv.test_type;
-var test_exe = "";
+var test_exe = argv.testexe;
+var cmd_args;
 
 if (typeof conn_string == 'undefined' || conn_string.length == 0) {
     console.log("Invalid connection string specified");
@@ -80,25 +107,12 @@ else {
         device_id = "prov_e2e_dev_" + uuid.v4();
     }
 
-    if (test_type == "x509") {
-        var x509_cert = argv.x509Cert;
-        if (typeof x509_cert == 'undefined' || x509_cert.length == 0) {
-            console.log("Invalid x509 certificate specified");
-        }
-        else {
-            prov_svc_client.create_x509_registration(conn_string, device_id, x509_cert, webhookuri);
-        }
-    }
-    else if (test_type == "symm_key") {
-        var symm_key = argv.symmKey;
-        prov_svc_client.create_symm_key_registration(conn_string, device_id, symm_key, webhookuri);
-    }
-    else if (test_type == "tpm") {
-        prov_svc_client.create_tpm_registration(conn_string, device_id, webhookuri);
-    }
+    register_dps_enrollment(test_type, conn_string, device_id, webhookuri, argv);
 
     if (test_exe.length > 0) {
-        execute_test_process(test_process, cmd_args);
+        execute_test_process(test_exe, cmd_args, function(result) {
+            console.log("process return is " + result);
+        });
     }
     // open the iot hub and wait for a telemetry message from the device
 }
